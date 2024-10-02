75dd5a218558696f186430d1da151a4cbd3bda52
Programistich <dzhos.oleksii@gmail.com>
2024-10-02 14:24:19 +0300

    Initial Commit

---
+++ OAuth Apple Google/OAuth_Apple_GoogleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAuth_Apple_GoogleApp.swift
+//  OAuth Apple Google
+//
+//  Created by Programistich on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct OAuth_Apple_GoogleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
